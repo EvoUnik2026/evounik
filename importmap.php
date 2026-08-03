07f37588c1fea44a0ad5133eab6f6b630a96d1af
FirasSahli2010 <feras.f.sahli@gmail.com>
2026-08-03 11:06:15 +0200

    correct login for dashboard
    Signed-off-by: FirasSahli2010 <feras.f.sahli@gmail.com>

--- importmap.php
+++ importmap.php
@@ -16,7 +16,13 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    '@symfony/stimulus-bundle' => [
+        'path' => '@symfony/stimulus-bundle/loader.js',
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '8.0.23',
     ],
 ];
--- src/Kernel.php
+++ src/Kernel.php
@@ -4,17 +4,6 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\MakerBundle\MakerBundle;
-use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
-use Symfony\UX\Turbo\TurboBundle;
-use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use Symfony\Bundle\DebugBundle\DebugBundle;
 
 class Kernel extends BaseKernel
 {
@@ -22,18 +11,12 @@ class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
-        yield new FrameworkBundle();
-        yield new TwigBundle();
-        yield new SecurityBundle();
-        yield new DoctrineBundle();
-        yield new DoctrineMigrationsBundle();
-        yield new MonologBundle();
-        yield new MakerBundle();
-        yield new TurboBundle();
-        
-        if ($this->getEnvironment() === 'dev') {
-            yield new WebProfilerBundle();
-            yield new DebugBundle();
+        $contents = require $this->getProjectDir().'/config/bundles.php';
+
+        foreach ($contents as $class => $envs) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+                yield new $class();
+            }
         }
     }
 }
