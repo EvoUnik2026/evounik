969506cb98e1955ddb8537a0aadcd5b8ac385c04
FirasSahli2010 <feras.f.sahli@gmail.com>
2026-08-09 11:57:05 +0200

    design and asset auto compile issues
    Signed-off-by: FirasSahli2010 <feras.f.sahli@gmail.com>

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,9 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
 import './styles/app.css';
-import './styles/homepage.css'
+import './styles/homepage.css';
 import './styles/topic_detail.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
